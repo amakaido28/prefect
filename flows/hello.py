b6e7d1448875c58a4de60ecd563a5fb55eb6aaaa
amakaido28 <appesiti@gmail.com>
2024-10-15 13:35:03 +0000

    minor

--- flows/hello.py
+++ flows/hello.py
@@ -1,14 +1,9 @@
 from prefect import flow, tags
 from prefect.logging import get_run_logger
-import os 
-os.environ['PREFECT_API_URL'] = 'http://172.18.21.116:4200/api'
-os.environ['PREFECT_SERVER_API_HOST'] = '172.18.21.116'
-os.environ['PREFECT_SERVER_ALLOW_EPHEMERAL_MODE'] = 'False'
 
 @flow
 def hello(name: str = "Marvin"):
-    logger = get_run_logger()
-    logger.info(f"Hello, {name}!")
+    print("!*!*!*!*!* Ciao, primo print *!*!**!*!*!*!")
 
 if __name__ == "__main__":
     hello.from_source(
